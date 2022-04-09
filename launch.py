27e2efbf50578722d046eefc8032da81ba66b14b
iiPython <ben@iipython.cf>
2022-04-09 16:58:35 -0500

    Add support for PORT environment var

--- launch.py
+++ launch.py
@@ -1,6 +1,7 @@
 # Copyright 2022 iiPython
 
 # Modules
+import os
 import sys
 
 # Initialization
@@ -13,6 +14,6 @@ from nebula import app
 if __name__ == "__main__":
     app.run(
         host = "0.0.0.0",
-        port = 8080,
+        port = int(os.getenv("PORT", 8080)),
         debug = True
     )
